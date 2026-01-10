0464d782bb65b60a3ce43e4c563936c2ecfdbaba
Sylvangh <sylvandoloritoyt@gmail.com>
2026-01-10 20:17:17 +0800

    Update authController.php

--- authController.php
+++ authController.php
@@ -201,9 +201,9 @@ elseif ($action === "adminSaveResident") {
         "pwd" => ($_POST['pwd'] ?? 'No') === 'Yes' ? 'Yes' : 'No',
         "fourps" => ($_POST['fourps'] ?? 'No') === 'Yes' ? 'Yes' : 'No',
         // ---------------- CHECKBOXES ----------------
-        "seniorcitizen" => (isset($_POST['seniorCitizen']) && $_POST['seniorCitizen'] === '1') ? 'Yes' : 'No',
-        "vaccinated" => (isset($_POST['vaccinated']) && $_POST['vaccinated'] === '1') ? 'Yes' : 'No',
-        "voter" => (isset($_POST['voter']) && $_POST['voter'] === '1') ? 'Yes' : 'No',
+        "seniorcitizen" => isset($_POST['seniorcitizen']) && $_POST['seniorcitizen'] === '1' ? 1 : 0,
+        "vaccinated" => isset($_POST['vaccinated']) && $_POST['vaccinated'] === '1' ? 1 : 0,
+        "voter" => isset($_POST['voter']) && $_POST['voter'] === '1' ? 1 : 0,
         // ---------------- SCHOOL ----------------
         "schoollevels" => !empty($_POST['schoollevels']) ? (is_array($_POST['schoollevels']) ? implode(',', $_POST['schoollevels']) : $_POST['schoollevels']) : '',
         "schoolname" => $_POST['schoolname'] ?? '',
@@ -281,6 +281,7 @@ else {
 
 echo json_encode($response);
 exit();
+
 
 
 
