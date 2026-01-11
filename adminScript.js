203c2c2222cd618470b7cfb22fbd00ba8a337901
Sylvangh <sylvandoloritoyt@gmail.com>
2026-01-11 11:39:24 +0800

    Update adminScript.js

--- adminScript.js
+++ adminScript.js
@@ -595,7 +595,7 @@ function renderTable(data) {
       <td>${r.occupation ?? ""}</td>
       <td>${vaccinated}</td>
       <td>${voter}</td>
-    <td>${validIdPath ? `<img src="${validIdPath}" width="50" />` : ""}</td>
+  <td>${r.validid ? `<img src="${window.location.origin}/${r.validid}" width="50" />` : ""}</td>
       <td>
         <button class="editBtn" data-id="${r.id}">Edit</button>
         <button class="deleteBtn" data-id="${r.id}">Delete</button>
@@ -1712,6 +1712,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Default page
   loadDashboard();
 });
+
 
 
 
