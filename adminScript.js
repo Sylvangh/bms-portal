fae359cd0f84a0607d7dcf5c2998fea9786c3d2c
Sylvangh <sylvandoloritoyt@gmail.com>
2026-01-12 10:07:44 +0800

    Update adminScript.js

--- adminScript.js
+++ adminScript.js
@@ -1332,7 +1332,7 @@ function renderTable(data = tableData) {
       juniorHigh: "Junior High",
       elementary: "Elementary"
     };
-    data = data.filter(r => (r.schoolLevels || []).includes(levelMap[selectedFilter]));
+    data = data.filter(r => (r.schoollevels || []).includes(levelMap[selectedFilter]));
   }
       // 30+ AGE filter
  // 30+ AGE filter
@@ -1404,9 +1404,9 @@ if (deleteMode && customColumns.includes(c)) {
 
       if (["college", "seniorHigh", "juniorHigh", "elementary"].includes(c)) {
         const levelMap = { college: "College", seniorHigh: "Senior High", juniorHigh: "Junior High", elementary: "Elementary" };
-        td.textContent = (row.schoolLevels || []).includes(levelMap[c]) ? "Yes" : "No";
+        td.textContent = (row.schoollevels || []).includes(levelMap[c]) ? "Yes" : "No";
         td.contentEditable = false;
-      } else if (["voter", "seniorCitizen"].includes(c)) {
+      } else if (["voter", "seniorcitizen"].includes(c)) {
         td.textContent = (row[c] === 1 || row[c] === "1") ? "Yes" : "No";
         td.contentEditable = false;
       } else {
@@ -1741,6 +1741,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Default page
   loadDashboard();
 });
+
 
 
 
